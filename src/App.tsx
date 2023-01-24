b6effd49f34c71649299b405083e2b98240044a1
CodeSandbox <support@codesandbox.io>
2023-01-24 12:19:13 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import { ReactComponentElement } from "react";
+import "./App.css";
+
+function ProductCategoryRow({ category }) {
+  return (
+    <tr>
+      <th colSpan={2}>{category}</th>
+    </tr>
+  );
+}
+
+function ProductRow({ product }) {
+  // stockedがfalseなら赤文字にする
+  const name = product.stocked ? (
+    product.name
+  ) : (
+    <span style={{ color: "red" }}>{product.name}</span>
+  );
+
+  return (
+    <tr>
+      <td>{name}</td>
+      <td>{product.price}</td>
+    </tr>
+  );
+}
+
+function ProductTable({ products }) {
+  const rows = [];
+  let lastCategory: string | null = null;
+
+  products.forEach((product) => {
+    if (product.category !== lastCategory) {
+      rows.push(
+        <ProductCategoryRow
+          category={product.category}
+          key={product.category}
+        />
+      );
+    }
+    rows.push(<ProductRow product={product} key={product.name} />);
+    lastCategory = product.category;
+  });
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
